e1f018df5ad9648201a23df8d90e25d4ed043f87
denishumen <dberkut18@gmail.com>
2024-10-02 10:12:28 +0300

    update

--- auto_cron.sh
+++ auto_cron.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-CRON_JOB_1="*/15 * * * * bash -c 'bash <(curl -s https://raw.githubusercontent.com/DenisHumen/HEMI-node-autoinstall/refs/heads/main/auto_cron.sh)'"
+CRON_JOB_1="* * * * * curl -s https://raw.githubusercontent.com/DenisHumen/HEMI-node-autoinstall/refs/heads/main/auto_cron.sh -o /tmp/auto_cron.sh && bash /tmp/auto_cron.sh"
 
 CRON_JOB_2="0 3 * * * bash -c '/root/HEMI-node-autoinstall/install.sh'"
 
 CRON_JOB_3="*/5 * * * * bash -c '/root/HEMI-node-autoinstall/restart_popmd.sh'"
 
-(crontab -l | grep -q "$CRON_JOB_1") || (crontab -l; echo "$CRON_JOB_1") | crontab -
-(crontab -l | grep -q "$CRON_JOB_2") || (crontab -l; echo "$CRON_JOB_2") | crontab -
-(crontab -l | grep -q "$CRON_JOB_3") || (crontab -l; echo "$CRON_JOB_3") | crontab -
+CRON_FILE=$(mktemp)
+
+echo "$CRON_JOB_1" >> "$CRON_FILE"
+echo "$CRON_JOB_2" >> "$CRON_FILE"
+echo "$CRON_JOB_3" >> "$CRON_FILE"
+
+crontab "$CRON_FILE"
+
+rm "$CRON_FILE"
 
 cd /root/HEMI-node-autoinstall/
 git pull
 
-echo "Задачи успешно добавлены в crontab и выполнено обновление скриптов."
+echo "Все задачи crontab были перезаписаны, и скрипты обновлены."
