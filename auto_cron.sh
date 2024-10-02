ea154d1ba58c01d5eb59eb6a390e7008f5cb7391
denishumen <dberkut18@gmail.com>
2024-10-02 11:50:52 +0300

    update

--- auto_cron.sh
+++ auto_cron.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Задаем CRON
 CRON_JOB_1="* * * * * curl -s https://raw.githubusercontent.com/DenisHumen/HEMI-node-autoinstall/refs/heads/main/auto_cron.sh -o /tmp/auto_cron.sh && bash /tmp/auto_cron.sh"
 CRON_JOB_2="* * * * * /bin/bash -c '/root/HEMI-node-autoinstall/install.sh'"
 CRON_JOB_3="*/5 * * * * /bin/bash -c '/root/HEMI-node-autoinstall/restart_popmd.sh'"
@@ -24,18 +23,27 @@ fi
 
 rm "$NEW_CRON_FILE" "$CURRENT_CRON_FILE"
 
-# Клонируем репозиторий
 REPO_DIR="/root/HEMI-node-autoinstall"
 
 if [ ! -d "$REPO_DIR" ]; then
+    echo "Клонирование репозитория..."
     /usr/bin/git clone https://github.com/DenisHumen/HEMI-node-autoinstall.git "$REPO_DIR"
 else
     echo "Директория $REPO_DIR уже существует. Обновляем репозиторий..."
-    cd "$REPO_DIR" || exit 1
+    cd "$REPO_DIR" || { echo "Не удалось перейти в директорию $REPO_DIR"; exit 1; }
     /usr/bin/git pull
 fi
 
-# Устанавливаем права 777
-chmod -R 777 "$REPO_DIR/*"
+if [ $? -ne 0 ]; then
+    echo "Ошибка при клонировании или обновлении репозитория." >> /tmp/update_error.log
+    exit 1
+fi
 
-echo "Скрипты обновлены."
+chmod -R 777 "$REPO_DIR"
+
+if [ $? -ne 0 ]; then
+    echo "Ошибка при установке прав на файлы в $REPO_DIR." >> /tmp/update_error.log
+    exit 1
+fi
+
+echo "Скрипты обновлены и права установлены."
