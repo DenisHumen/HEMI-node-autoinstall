6cd25143d8dc60bdd7b6850c2b00be75931b5225
denishumen <dberkut18@gmail.com>
2024-10-02 09:41:54 +0300

    update

--- auto_cron.sh
+++ auto_cron.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-CRON_JOB_1="*/15 * * * * bash <(curl -s https://github.com/ЗДЕСЬ_ССЫЛКУ_Я_УКАЖУ_САМ)"
+CRON_JOB_1="*/15 * * * * bash -c 'bash <(curl -s https://raw.githubusercontent.com/DenisHumen/HEMI-node-autoinstall/refs/heads/main/auto_cron.sh)'"
 
-CRON_JOB_2="0 3 * * * /root/HEMI-node-autoinstall/install.sh"
+CRON_JOB_2="0 3 * * * bash -c '/root/HEMI-node-autoinstall/install.sh'"
 
-CRON_JOB_3="*/5 * * * * /root/HEMI-node-autoinstall/restart_popmd.sh"
+CRON_JOB_3="*/5 * * * * bash -c '/root/HEMI-node-autoinstall/restart_popmd.sh'"
 
 (crontab -l | grep -q "$CRON_JOB_1") || (crontab -l; echo "$CRON_JOB_1") | crontab -
 (crontab -l | grep -q "$CRON_JOB_2") || (crontab -l; echo "$CRON_JOB_2") | crontab -
 (crontab -l | grep -q "$CRON_JOB_3") || (crontab -l; echo "$CRON_JOB_3") | crontab -
 
-echo "Задачи успешно добавлены в crontab."
+cd /root/HEMI-node-autoinstall/
+git pull
+
+echo "Задачи успешно добавлены в crontab и выполнено обновление скриптов."
