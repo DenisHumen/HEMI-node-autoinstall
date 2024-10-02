f0f9ff161a174e080c8b793cacec1990bdb1f1d5
denishumen <dberkut18@gmail.com>
2024-10-02 09:19:44 +0300

    update

--- restart_popmd.sh
+++ restart_popmd.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-service popmd restart # добавить в крон, желательно на каждые 4 часа 
+SERVICE="popmd.service"
+
+check_service_status() {
+    STATUS=$(systemctl is-active "$SERVICE")
+
+    if [ "$STATUS" != "active" ]; then
+        echo "Сервис $SERVICE не активен. Выполняется перезапуск..."
+        systemctl restart "$SERVICE"
+        
+        if [ "$(systemctl is-active "$SERVICE")" == "active" ]; then
+            echo "Сервис $SERVICE успешно перезапущен."
+        else
+            echo "Не удалось перезапустить сервис $SERVICE."
+        fi
+    else
+        echo "Сервис $SERVICE работает корректно."
+    fi
+}
+
+check_service_status
