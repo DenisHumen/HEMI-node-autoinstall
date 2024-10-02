15a936a059c43ead147405faf5070adb36e55ea7
denishumen <dberkut18@gmail.com>
2024-10-02 10:25:44 +0300

    update

--- auto_cron.sh
+++ auto_cron.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
 CRON_JOB_1="* * * * * curl -s https://raw.githubusercontent.com/DenisHumen/HEMI-node-autoinstall/refs/heads/main/auto_cron.sh -o /tmp/auto_cron.sh && bash /tmp/auto_cron.sh"
-
 CRON_JOB_2="0 3 * * * bash -c '/root/HEMI-node-autoinstall/install.sh'"
-
 CRON_JOB_3="*/5 * * * * bash -c '/root/HEMI-node-autoinstall/restart_popmd.sh'"
 
-CRON_FILE=$(mktemp)
+NEW_CRON_FILE=$(mktemp)
 
-echo "$CRON_JOB_1" >> "$CRON_FILE"
-echo "$CRON_JOB_2" >> "$CRON_FILE"
-echo "$CRON_JOB_3" >> "$CRON_FILE"
+echo "$CRON_JOB_1" >> "$NEW_CRON_FILE"
+echo "$CRON_JOB_2" >> "$NEW_CRON_FILE"
+echo "$CRON_JOB_3" >> "$NEW_CRON_FILE"
 
-crontab "$CRON_FILE"
+CURRENT_CRON_FILE=$(mktemp)
 
-rm "$CRON_FILE"
+crontab -l > "$CURRENT_CRON_FILE" 2>/dev/null
+
+if ! cmp -s "$NEW_CRON_FILE" "$CURRENT_CRON_FILE"; then
+    crontab "$NEW_CRON_FILE"
+    echo "Обнаружены изменения. Задачи crontab были перезаписаны."
+else
+    echo "Изменений в crontab нет. Перезапись не требуется."
+fi
+
+rm "$NEW_CRON_FILE" "$CURRENT_CRON_FILE"
 
 cd /root/HEMI-node-autoinstall/
 git pull
 
-echo "Все задачи crontab были перезаписаны, и скрипты обновлены."
+echo "Скрипты обновлены."
